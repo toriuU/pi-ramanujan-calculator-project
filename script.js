2751c4157882dd0f042b248cc2a60672c8c48f70
Vittorio <vittorionimeth12@gmail.com>
2026-09-16 19:52:20 -0300

    Calculate the result of the formula in number format

--- script.js
+++ script.js
@@ -3,23 +3,26 @@ const display = document.querySelector("#displayPi #displayText");
 let clicksButton = 0;
 let valuePi;
 const firstProductFormula = 2*(2**(1/2)) / 9801;
-let Summation;
-let SummationFirstProduct, SummationSecondProduct;
+let formulaProduct = BigInt(0);
+let summationFirstFactor, summationSecondFactor;
+let summationProduct = 0;
 
 function fatorial(n) {
     let result = 1;
 
-    for(let i = 1; i <= n; ++i) {
+    for (let i = 1; i <= n; ++i) {
         result *= i;
     };
     return result;
 };
 
 button.addEventListener("click", () => {
-    clicksButton += 1;
-    console.log(clicksButton);
+    clicksButton++;
+    for (let i = 0; i < clicksButton; i++) {
+        summationFirstFactor = (i*26390 + 1103) / ((fatorial(i))**4);
+        summationSecondFactor = (fatorial(4*i)) / (396**(4*i));
+        summationProduct += summationFirstFactor * summationSecondFactor;
+        formulaProduct = (1 / (summationProduct * firstProductFormula));
+    };
+    summationProduct = 0;
 });
-
-for(let i = 0; i = clicksButton; i++) {
-
-}
