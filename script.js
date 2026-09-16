d252a9e10836fe2dd354444be01d4f6d26f5900b
Vittorio <vittorionimeth12@gmail.com>
2026-09-16 15:48:53 -0300

    Do the acting of cliking button add 1 in clicksButton

---
+++ script.js
@@ -0,0 +1,16 @@
+const button = document.querySelector("#buttonMinePi");
+const display = document.querySelector("#displayPi #displayText");
+let clicksButton = 0;
+let valuePi;
+const firstProductFormula = 2*(2**(1/2)) / 9801;
+let Summation;
+let SummationFirstProduct, SummationSecondProduct;
+
+button.addEventListener("click", () => {
+    clicksButton += 1;
+    console.log(clicksButton);
+});
+
+for(let i = 0; i = clicksButton; i++) {
+
+}
